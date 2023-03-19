11cf9ae3cdcccd727354da25a6e48a3de564af39
seikou <seikou615@gmail.com>
2023-03-19 13:27:18 +0000

    Added: ErrorBoundary class component

---
+++ src/components/ErrorBoundary/index.tsx
@@ -0,0 +1,30 @@
+import { Component, ErrorInfo, ReactNode } from "react";
+import { IProps, IState } from "./types";
+
+class ErrorBoundary extends Component<IProps, IState> {
+  public state: IState = {
+    hasError: false,
+  };
+
+  public static getDerivedStateFromError(_: Error): IState {
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return (
+        <main>
+          <h1>Sorry.. there was an error</h1>;
+        </main>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
