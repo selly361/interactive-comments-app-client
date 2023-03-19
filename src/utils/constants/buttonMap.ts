87f67b8f2adad409f80cfb5ee003eeb22c7ff817
seikou <seikou615@gmail.com>
2023-03-19 13:22:00 +0000

    Added: buttonMap in constants folder

---
+++ src/utils/constants/buttonMap.ts
@@ -0,0 +1,11 @@
+import {
+    PrimaryButton,
+    SecondaryButton,
+    TertiaryButton,
+} from "components/shared/Button/Button.styles";
+
+export const buttonMap = {
+    'primary-button': PrimaryButton,
+    'secondary-button': SecondaryButton,
+    'tertiary-button': TertiaryButton,
+  };
