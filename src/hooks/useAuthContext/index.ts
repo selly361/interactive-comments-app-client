5ee3178579f2af7da94e7ef3d9a45371ae6d9b95
selly361 <104414846+selly361@users.noreply.github.com>
2023-03-27 11:53:02 +0000

    Added: `useAuthContext` hook to simplify using the `authContext` api

---
+++ src/hooks/useAuthContext/index.ts
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react'
+
+import { AuthContext } from 'contexts/AuthContext'
+
+export const useAuthContext = () => {
+   const context = useContext(AuthContext)
+   if (context === undefined) {
+      throw new Error('useAuthContext must be used within a AuthContextProvider')
+   }
+
+   return context
+}
