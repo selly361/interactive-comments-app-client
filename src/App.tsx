10eac59288f517e7de2cc577ecdaa815fb7b30ef
seikou <seikou615@gmail.com>
2023-03-28 11:58:59 +0100

    Added: default code for react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,17 @@
-import AuthContextProvider, { AuthContext } from 'contexts/AuthContext'
-import React, { Fragment, useContext } from 'react'
+import { HomePage, LoginPage, RegisterPage } from 'pages'
+import { Route, Routes } from 'react-router-dom'
 
-import { GlobalStyles } from 'global/index.GlobalStyles'
+import { Wrapper } from 'components'
 
 function App() {
    return (
-      <Fragment>
-         <GlobalStyles />
-         <AuthContextProvider>
-            <Test />
-         </AuthContextProvider>
-      </Fragment>
-   )
-}
-
-function Test() {
-   const { login, signup, user, logout } = useContext(AuthContext)
-
-   return (
-      <div>
-         <button onClick={() => login('seikou615@gmail.com', 'sillah123')}>
-            <h1>LOGIN</h1>
-         </button>
-         <br />
-         <button onClick={() => signup('seikou615@gmail.com', 'aimerno', 'sillah123')}>
-            <h1>REGISTER</h1>
-         </button>
-         <button onClick={() => logout()}>
-            <h1>LOGOUT</h1>
-         </button>
-         <div>{JSON.stringify(user)}</div>
-         <div dangerouslySetInnerHTML={{ __html: user?.profile_image || '' }} />
-      </div>
+      <Wrapper>
+         <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+         </Routes>
+      </Wrapper>
    )
 }
 
