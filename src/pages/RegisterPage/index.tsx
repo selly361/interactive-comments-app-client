123bce73c794ea59ef7a714a5f024ec50cc6edf0
seikou <seikou615@gmail.com>
2023-03-28 12:00:19 +0100

    Added: default pages and exported them as default

---
+++ src/pages/RegisterPage/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { RegisterForm } from 'components/Forms'
+
+function RegisterPage() {
+  return (
+    <div>RegisterPage</div>
+  )
+}
+
+export default RegisterPage
