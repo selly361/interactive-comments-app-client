36076fc91fa372a2b618862ff9e11169173a179e
seikou <seikou615@gmail.com>
2023-03-29 18:53:04 +0100

    Add: used `react-hook-form` in both `LoginForm` and `RegisterForm` components and created a type file for each one

--- src/components/Forms/Login/index.tsx
+++ src/components/Forms/Login/index.tsx
@@ -1,9 +1,62 @@
+import { ILoginForm } from './Login.types'
 import React from 'react'
+import { useAuthContext } from 'hooks/useAuthContext'
+import { useForm } from 'react-hook-form'
 
 function LoginForm() {
-  return (
-    <div>LoginForm</div>
-  )
+   const {
+      register,
+      handleSubmit,
+      formState: { errors },
+   } = useForm<ILoginForm>()
+
+   const { login, isAuthenticated } = useAuthContext()
+
+   const onSubmit = (data: ILoginForm) => {
+      login(data.email, data.password)
+   }
+
+   if (isAuthenticated) {
+   }
+
+   return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+         <div>
+            <label htmlFor="email">Email</label>
+            <input
+               type="text"
+               id="email"
+               {...register('email', {
+                  required: 'Email is required',
+                  pattern: {
+                     value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                     message: 'Invalid email address',
+                  },
+               })}
+            />
+            {errors.email && <span>{errors.email.message}</span>}
+         </div>
+
+         <div>
+            <label htmlFor="password">Password</label>
+            <input
+               type="password"
+               id="password"
+               {...register('password', {
+                  required: 'Password is required',
+                  pattern: {
+                     value: /^[a-zA-Z0-9]{5,20}$/,
+                     message:
+                        'Password must be between 5 and 20 characters and contain only letters and numbers',
+                  },
+               })}
+            />
+            {errors.password && <span>{errors.password.message}</span>}
+         </div>
+
+         <button type="submit">Login</button>
+      </form>
+   )
 }
 
 export default LoginForm
--- src/components/Forms/Register/index.tsx
+++ src/components/Forms/Register/index.tsx
@@ -1,8 +1,65 @@
 import React from 'react'
+import { useAuthContext } from 'hooks/useAuthContext'
+import { useForm } from 'react-hook-form'
+
+interface RegisterFormProps {
+  onSubmit: (data: RegisterFormData) => void
+}
+
+interface RegisterFormData {
+  email: string
+  password: string
+  username: string
+}
 
 function RegisterForm() {
+  const { register, handleSubmit, formState: { errors } } = useForm<RegisterFormData>();
+  const { signup } = useAuthContext()
+  
+  const onSubmitHandler = handleSubmit((data: RegisterFormData) => {
+    signup(data.email, data.username, data.password)
+  })
+
   return (
-    <div>RegisterForm</div>
+    <form onSubmit={onSubmitHandler}>
+      <div>
+        <label>Email</label>
+        <input {...register("email", {
+          required: true,
+          pattern: {
+            value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            message: 'Invalid email format'
+          }
+        })} />
+        {errors.email && <span>{errors.email.message}</span>}
+      </div>
+
+      <div>
+        <label>Password</label>
+        <input {...register("password", {
+          required: true,
+          pattern: {
+            value: /^[a-zA-Z0-9]{5,20}$/,
+            message: 'Password should be 5-20 alphanumeric characters'
+          }
+        })} />
+        {errors.password && <span>{errors.password.message}</span>}
+      </div>
+
+      <div>
+        <label>Username</label>
+        <input {...register("username", {
+          required: true,
+          pattern: {
+            value: /^[A-Za-z]\w{3,13}[A-Za-z0-9]$/,
+            message: 'Username should be 4-14 alphanumeric characters starting with a letter'
+          }
+        })} />
+        {errors.username && <span>{errors.username.message}</span>}
+      </div>
+
+      <button type="submit">Register</button>
+    </form>
   )
 }
 
