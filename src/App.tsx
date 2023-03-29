248736daeb46ffde6235dd342ed71ead37ce3a41
seikou <seikou615@gmail.com>
2023-03-29 18:48:29 +0100

    Change: used `Anonymouse` component in routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
+import { Anonymous, Wrapper } from 'components'
 import { HomePage, LoginPage, RegisterPage } from 'pages'
 import { Route, Routes } from 'react-router-dom'
-
-import { Wrapper } from 'components'
 
 function App() {
    return (
       <Wrapper>
          <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/login" element={<LoginPage />} />
+            <Route element={<Anonymous />}>
+               <Route path="/register" element={<RegisterPage />} />
+               <Route path="/login" element={<LoginPage />} />
+            </Route>
          </Routes>
       </Wrapper>
    )
