ab7073b673dbde6b6f6dbc716966b94995a755f0
selly361 <104414846+selly361@users.noreply.github.com>
2023-03-30 11:06:14 +0000

    Added: `Loading` component in shared folder

---
+++ src/shared/components/Loading/index.tsx
@@ -0,0 +1,13 @@
+import { LoadingCircle, StyledLoading } from './Loading.styles'
+
+import React from 'react'
+
+function Loading() {
+   return (
+      <StyledLoading>
+         <LoadingCircle>Loading...</LoadingCircle>
+      </StyledLoading>
+   )
+}
+
+export default Loading
