22927619cb85bdc394c0495bcd3a8b30d6b60b63
selly361 <104414846+selly361@users.noreply.github.com>
2023-03-30 11:41:33 +0000

    Exported `Header` component as default

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Wrapper } from "./Wrapper"
 export { default as ErrorBoundary } from "./ErrorBoundary"
 export { default as Anonymous } from "./Anonymous"
+export { default as Header } from "./Header"
